2e3542a5ff1cf361abc44e51622c5e3fd0a76865
Michael Bishop <michael@openads.ai>
2025-04-21 19:00:18 -0400

    merge

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,38 +20,59 @@ export default function Home() {
     // @ts-ignore
     window.stratosSettings = {
       publisherId: '6660be5c4e70d17b07751c91', // prod
-      //publisherId: '63e57237d78d35eeaab15162', //sliss-dev
+      //publisherId: '67eccb065b9d76931e445efa', // liner
+      //publisherId: '63e57237d78d35eeaab15162', // sliss-dev
       //publisherId: '66393bf0c2ae100e268f0373', // michael@openads.ai demo account
-      //publisherId: '63e57237d78d35eeaab15162', //sliss-dev
       disableInitialLoad: true,
       adSlots: [
         {
           // adUnitCode: 'clarity-demo-chat-ad',
           adUnitCode: 'demo-clarity-chat-ad-infeed',
           adFormat: 'chat',
-          size: 'fluid',
+          size: ['970', '250']
         }
       ],
       apiEndpoint: '/api/proxy-ads',
-      cssOverrides:
-        `:root {
-        --background: #f5efdd;
-        --text: #000;
-        --header: #ffffff4c;
-        --header-background: #ffffff10;
-        --title: #000;
-        --highlight: #c8ddff;
-        --action-button: #3b82f6;
-        --question-bubble: #FFF;
-        --radius: 5px;
-        --user-chat: #0061ff;
-        --system-chat: #e0e0e0;
+      cssOverrides: ` :root {
+        --background: #ffffff;
+        --text: #303030;
+        --header: #666666;
+        --header-background: #f5f5f5;
+        --title: #000000;
+        --question-bubble: #f5f5f5;
+        --highlight: #4058FF;
+        --action-button: #4058FF;
+        --user-chat: #4058FF;
+        --system-chat: #f5f5f5;
+        --bubble-dot-color: #4b4b4b;
+        --radius: 6px;
       }
       #visit-site-link {
-        color: #FFF;
+        border-radius: 6px;
+        text-align: center;
+        background-color: #4058FF;
+        color: #ffffff;
+        font-weight: 500;
+        padding: 12px 20px;
       }
-      .ad-questions li:hover {
-        color: #000;
+      .ad-input button {
+        color: #303030;
+        background-color: #f5f5f5;
+        border-radius: 6px;
+      }
+      .ad-input input {
+        background-color: #f5f5f5;
+        border: 1px solid #e0e0e0;
+        border-radius: 6px;
+      }
+      .ad-input-container {
+        background-color: #ffffff;
+      }
+      .ad-questions {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        gap: 5px;
       }`
     };
 
